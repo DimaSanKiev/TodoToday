495bfd539a2ce973334119eba79f2b4f79e19118
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-11 11:45:51 +0300

    add controller

---
+++ src/main/java/todotoday/web/controller/TaskController.java
@@ -0,0 +1,25 @@
+package todotoday.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import todotoday.model.Task;
+import todotoday.service.TaskService;
+import todotoday.service.UserService;
+
+@Controller
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping({"/", "/todo"})
+    public String taskList(Model model) {
+        Iterable<Task> tasks = taskService.findAll();
+        model.addAttribute("tasks", tasks);
+        return "todo";
+    }
+}
