a32231029df39e71942dab69a09f08dab536fa1b
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-11 13:41:38 +0300

    update User model, implement UserDetails interface, update import.sql script

--- src/main/java/todotoday/model/User.java
+++ src/main/java/todotoday/model/User.java
@@ -1,10 +1,17 @@
 package todotoday.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,43 +30,40 @@ public class User {
     @JoinColumn(name = "role_id")
     private Role role;
 
-    public Long getId() {
-        return id;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.getName()));
+        return authorities;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public String getPassword() {
+        return passwrod;
     }
 
+    @Override
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public String getPasswrod() {
-        return passwrod;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public void setPasswrod(String passwrod) {
-        this.passwrod = passwrod;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
+    @Override
     public boolean isEnabled() {
         return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,10 @@
+-- Insert roles
+INSERT INTO role (name) VALUES ('ROLE_USER');
+
+-- Insert users
+INSERT INTO user (username, enabled, password, role_id) VALUES ('user', TRUE, 'password', 1);
+INSERT INTO user (username, enabled, password, role_id) VALUES ('user2', TRUE, 'password', 1);
+
 -- Insert tasks
 INSERT INTO task (complete, description) VALUES (TRUE, 'Code Task entity');
 INSERT INTO task (complete, description) VALUES (FALSE, 'Discuss users and roles');
