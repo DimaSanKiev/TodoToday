0a4d06187eca8beda1e5d95ad4d10931aae8cc18
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-11 12:49:09 +0300

    add import.sql script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- Insert tasks
+INSERT INTO task (complete, description) VALUES (TRUE, 'Code Task entity');
+INSERT INTO task (complete, description) VALUES (FALSE, 'Discuss users and roles');
+INSERT INTO task (complete, description) VALUES (FALSE, 'Enable Spring Security');
+INSERT INTO task (complete, description) VALUES (FALSE, 'Test application');
