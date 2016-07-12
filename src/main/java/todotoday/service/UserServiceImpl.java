5b9c78a02adc8628ec92a21f61cf5665760956ca
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-12 22:33:15 +0300

    add configuration classes

--- src/main/java/todotoday/service/UserServiceImpl.java
+++ src/main/java/todotoday/service/UserServiceImpl.java
@@ -3,10 +3,12 @@ package todotoday.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import todotoday.dao.UserDao;
 import todotoday.model.User;
 
-public class UsesrServiceImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
 
