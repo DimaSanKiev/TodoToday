20193dde93143048621eba07bfc45703040faf90
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-07-13 21:59:27 +0300

    update SecurityConfig
    - use BCrypt hash passwords;
    - add CSRF protection.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,11 @@
 -- Insert roles
 INSERT INTO role (name) VALUES ('ROLE_USER');
 
--- Insert users
+-- Insert users (passwords are both 'password')
 INSERT INTO user (username, enabled, password, role_id)
-  VALUES ('user', TRUE, '$2a$08$wgwoMKfYl5AUE9QtP4OjheNkkSDoqDmFGjjPE2XTPLDe9xso/hy7u', 1);
+  VALUES ('user', TRUE, '$2a$08$SuE48E18izYdripeojcCMOp2kPhH6fupCmLFN3u1ojX5f3wgkCjoK', 1);
 INSERT INTO user (username, enabled, password, role_id)
-  VALUES ('user2', TRUE, '$2a$08$wgwoMKfYl5AUE9QtP4OjheNkkSDoqDmFGjjPE2XTPLDe9xso/hy7u', 1);
+  VALUES ('user2', TRUE, '$2a$08$SuE48E18izYdripeojcCMOp2kPhH6fupCmLFN3u1ojX5f3wgkCjoK', 1);
 
 -- Insert tasks
 INSERT INTO task (complete, description, user_id) VALUES (TRUE, 'Code Task entity', 1);
